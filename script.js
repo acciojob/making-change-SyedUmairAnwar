028e2fa60da9c2cab69e5731c311c5f789901514
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-22 17:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const makeChange = (c) => {
   // your name here
+
+	let change={
+		q:0,
+		d:0,
+		n:0,
+	    p:0,
+	}
+	let count=0
+	while (c>=25) {
+		change.q=count++;
+		c= c-25;
+	}
+	count=0;
+	while (c>=10) {
+		change.d=count++;
+		c= c-10;
+	}
+	count=0;
+	while (c>=5) {
+		change.n=count++;
+		c= c-5;
+	}
+	count=0;
+	while (c>=1) {
+		change.p=count++;
+		c= c-1;
+	}
+	console.log(change);
 };
 
 // Do not the change the code below
